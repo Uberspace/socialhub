48d6f0cd68772a033bd02847e352efdbfeb1d9f2
luto <m@luto.at>
2020-05-14 12:38:22 +0200

    v1.0.0
    nothing special happening here, just semver

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ root_dir = path.abspath(path.dirname(__file__))
 
 setup(
     name='socialhub',
-    version='0.4.0',
+    version='1.0.0',
     description='API client for socialhub.io',
     author='uberspace.de',
     author_email='hallo@uberspace.de',
